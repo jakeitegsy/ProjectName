dbe375fab137887f6f1b49bb9992c16d6519747a
jakeitegsy <jakeitegsy@yahoo.com>
2021-02-28 22:27:15 -0600

    Create Custom InlineLambdaConstruct

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
-export class Hello {
-  public sayHello() {
-    return 'hello, world!';
+import { Code, Function, Runtime } from '@aws-cdk/aws-lambda';
+import { Construct, Duration } from '@aws-cdk/core';
+
+export class InlineLambdaConstruct extends Construct {
+  constructor(parent: Construct, name: string) {
+    super(parent, name);
+
+    new Function(this, 'SampleFunction', {
+      runtime: Runtime.NODEJS_12_X,
+      code: Code.fromInline('exports.handler = function(event, context, callback) { console.log("Event: ", event); callback(); };'),
+      handler: 'index.handler',
+      timeout: Duration.seconds(10),
+    });
   }
 }
