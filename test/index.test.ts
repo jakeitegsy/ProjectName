ae912ad7d8fe5efa28ec88520563087a309ffe1c
jakeitegsy <jakeitegsy@yahoo.com>
2021-02-28 22:28:45 -0600

    Add test

---
+++ test/index.test.ts
@@ -0,0 +1,12 @@
+import { countResources, expect as expectCDK } from '@aws-cdk/assert';
+import { App, Stack } from '@aws-cdk/core';
+import { InlineLambdaConstruct } from '../src';
+
+test('Simple test', () => {
+  const app = new App();
+  const stack = new Stack(app, 'TestStack');
+
+  new InlineLambdaConstruct(stack, 'SimpleInlineLambdaConstruct');
+
+  expectCDK(stack).to(countResources('AWS::Lambda::Function', 1));
+})
